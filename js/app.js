6cc6b91a7182baabedc7bbc3b5b1fff8942d5b5b
masumbillah360 <masumbillah6584@gmail.com>
2022-08-23 14:04:46 +0600

    added common function on js file and removing utilities file from html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
+// All are common function to use app.js file 
+// function for get input value using id
+function getInputValue(id) {
+    const value = document.getElementById(id).value;
+    return value;
+}
+// function for get innerText usign id
+function getInnerText(id) {
+    const stringText = document.getElementById(id).innerText;
+    const amount = parseFloat(stringText);
+    return amount;
+}
+
+// function for set input value in inner text element
+function setTextValue(id,value) {
+    const findElement = document.getElementById(id).innerText = value;
+    return findElement;
+}
+
+// function for large floating amount
+function fixedAmout(amount) {
+    const totalAmount = amount.toFixed(2);
+    const finalAmount = parseFloat(totalAmount);
+    return finalAmount;
+}
+
+//create a function for validation value 
+function validationValue(value) {
+    if (isNaN(value) || value.length<=0 || value<0 ){
+        alert("Please Enter Valied Amount");
+        return;
+    }
+    else{
+        const output = parseFloat(value);
+        return output;
+    }
+}
+// common function end's here
+
+
 // parent element ul for contain players list
 const playerNameContainer = document.getElementById('player-name-container');
 
